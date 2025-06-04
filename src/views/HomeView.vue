713fd8b2a70c81b8806270dd810188c4e6f2b8c3
Kisora Thomas <kisora.thomas@gmail.com>
2025-06-04 12:32:26 -0600

    Remove unused components and API-related utilities
    Deleted `AnimatedElements` component and `geminiApi` utility as they are no longer required. Updated `HomeView` to remove references to the removed component for cleaner and leaner code.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { useRouter } from 'vue-router'
-import AnimatedElements from '../components/AnimatedElements.vue'
 import SquareAnimation from '../components/SquareAnimation.vue'
 
 const router = useRouter()
